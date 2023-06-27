83b00d82ddfb7aede21a680c6cd7aecf675f7244
brianafirsta <100469506+brianafirsta@users.noreply.github.com>
2023-06-27 23:41:36 +0700

    Update app.js

--- sIngIng/app.js
+++ sIngIng/app.js
@@ -29,10 +29,12 @@ const upload = multer({
 });
 
 
+
 //buat folder penampung file jika tidak ada
 if (!fs.existsSync('./uploads')) {
   fs.mkdirSync('./uploads');
 }
+
 
 
 const saltRounds = 10;
@@ -71,9 +73,138 @@ const db = mysql.createConnection({
    console.log('db connected!!')
    })
 
+//memeriksa apakah pengguna telah terautentikasi sebelum memberikan akses ke rute berikutnya.
+function requireAuth(req, res, next) {
+  
+    const token = req.cookies.token;
+  
+    if (!token) {
+      res.redirect('/login');
+      return;
+    }
+    
+  
+    jwt.verify(token, 'secret_key', function(err, decoded) {
+      if (err) {
+        res.redirect('/login');
+        return;
+      }
+  
+      req.user_id = decoded.user_id;
+      next();
+    });
+  }
+
+// =================================================================
+//                         GET                                    //
+// =================================================================
+
+
+   app.get('/register', function (req, res) {
+    res.render('register',{
+      title : 'Register',
+      layout : 'layouts/autentikasi-layout'
+    });
+})
+
+   app.get('/login', function (req, res) {
+    res.render('login',{
+      title : 'Login',
+      layout : 'layouts/autentikasi-layout'
+    });
+})
+
+  app.get('/logout', function(req, res) {
+    res.clearCookie('token');
+    res.redirect('/login');
+  });
+
+//dashboard
+app.get('/', requireAuth, function (req, res) {
+    if (!req.user_id) {
+      res.redirect('/login');
+      return;
+    }
+    const user_id = req.user_id;
+  
+    const selectUserSql = `SELECT * FROM users WHERE user_id = ${user_id}`;
+    db.query(selectUserSql, (err, userResult) => {
+      if (err) throw err;
+  
+      const selectAllUsersSql = `SELECT * FROM users`;
+      db.query(selectAllUsersSql, (err, allUsersResult) => {
+        if (err) throw err;
+  
+        const selectAllDocumentsSql = `SELECT * FROM documents`;
+        db.query(selectAllDocumentsSql, (err, allDocumentsResult) => {
+          if (err) throw err;
+  
+          const selectAllSignaturesSql = `SELECT * FROM signature`;
+          db.query(selectAllSignaturesSql, (err, allSignaturesResult) => {
+            if (err) throw err;
+  
+            res.render('index', {
+              user: userResult[0],
+              allUsers: allUsersResult,
+              allDocuments: allDocumentsResult,
+              allReq: allSignaturesResult,
+              title: 'Dashboard',
+              layout: 'layouts/main'
+            });
+          });
+        });
+      });
+    });
+  });
+
+// Profile
+app.get('/profil', requireAuth, function (req, res) {
+  let user_id = req.user_id;
+  const selectSql = `SELECT * FROM users WHERE user_id = ${user_id}`;
+  db.query(selectSql, (err,userResult)=>{
+    if (err) throw err;
+      res.render('profil',{
+        user: userResult[0],
+        title:'Profil',
+        layout:'layouts/main'
+      })
+  })
+})
+
+app.get('/add-document', requireAuth, function (req, res) {
+  let user_id = req.user_id;
+  const selectUserSql = `SELECT * FROM users WHERE user_id != ${user_id}`;
+  db.query(selectUserSql, (err, userResult) => {
+    if (err) throw err;
+    res.render('add-document', {
+      users: userResult,
+      title: 'add document',
+      layout: 'layouts/main'
+    });
+  });
+});
+
+
+  app.get('/signature', function (req, res) {
+    res.render('signature',{ 
+    title:"signature",
+    layout:"layouts/main"
+    }) 
+})
+
+  app.get('/action-req', function (req, res) {
+    res.render('action-req',{ 
+    title:"action required",
+    layout:"layouts/main"
+    }) 
+})
+
 //History (From-Others)
 app.get('/from-others', requireAuth, function (req, res) {
   let user_id = req.user_id;
+  const selectUserSql = `SELECT * FROM users WHERE user_id = ${user_id}`;
+  db.query(selectUserSql, (err, userResult) => {
+    if (err) throw err;
     const selectDocumentsSql =`
       SELECT documents.document_id, documents.name, documents.description
       FROM documents
@@ -83,10 +214,12 @@ app.get('/from-others', requireAuth, function (req, res) {
     db.query(selectDocumentsSql, (err, documentResult) => {
       if (err) throw err;
         res.render('from-others', {
+          user: userResult[0],
           documents: documentResult,
           moment: moment,
           title: 'from others',
           layout: 'layouts/main'
+      });
     });
   });
 });
@@ -113,19 +246,85 @@ app.get('/download/:document_id', requireAuth, (req, res) => {
   });
 });
 
-// Profile
-app.get('/profil', requireAuth, function (req, res) {
-  let user_id = req.user_id;
-  const selectSql = `SELECT * FROM users WHERE user_id = ${user_id}`;
-  db.query(selectSql, (err,userResult)=>{
+
+// =================================================================
+//                         POST                                    //
+// =================================================================
+
+app.post('/register', function (req, res) {
+  const { username, password, confirm_password } = req.body;
+
+  // check if username already exists
+  const sqlCheck = 'SELECT * FROM users WHERE username = ?';
+  db.query(sqlCheck, username, (err, result) => {
     if (err) throw err;
-      res.render('profil',{
-        user: userResult[0],
-        title:'Profil',
-        layout:'layouts/main'
-      })
-  })
-})
+
+    if (result.length > 0) {
+      console.error({ message: 'Username sudah terdaftar', err });
+      req.session.errorMessage = 'Username sudah terdaftar';
+      return res.redirect('/register');
+    }
+
+    if (password !== confirm_password) {
+      console.error({ message: 'Password tidak cocok!', err });
+      req.session.errorMessage = 'Password tidak cocok!';
+      return res.redirect('/register');
+    }
+
+    // hash password
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+      if (err) throw err;
+
+      // insert user to database
+      const sqlInsert = 'INSERT INTO users (username, password) VALUES (?, ?)';
+      const values = [username, hash];
+      db.query(sqlInsert, values, (err, result) => {
+        if (err) throw err;
+        console.log({ message: 'Registrasi berhasil', values });
+        res.redirect('/login');
+      });
+    });
+  });
+});
+
+app.post('/login', function (req, res) {
+  const { username, password } = req.body;
+
+  const sql = 'SELECT * FROM users WHERE username = ?';
+  db.query(sql, [username], function(err, result) {
+    if (err) {
+      console.error({ message: 'Internal Server Error', err });
+      return res.redirect('/login');
+    }
+
+    if (result.length === 0) {
+      console.error({ message: 'Username atau Password salah!!', err });
+      return res.redirect('/login');
+    }
+
+    const user = result[0];
+
+    // compare password
+    bcrypt.compare(password, user.password, function(err, isValid) {
+      if (err) {
+        console.error({ message: 'Internal Server Error', err });
+        return res.redirect('/login');
+      }
+
+      if (!isValid) {
+        console.error({ message: 'Username atau Password salah!!', err });
+        return res.redirect('/login');
+      }
+
+      // generate token
+      const token = jwt.sign({ user_id: user.user_id }, 'secret_key');
+      res.cookie('token', token, { httpOnly: true });
+
+      console.log({ message: 'Login Success', user });
+      return res.redirect('/');
+    });
+  });
+});
 
 // Profile (Update-Profile)
 app.post('/update-profil', upload.single('sign_img'), requireAuth, (req, res) => {
@@ -194,43 +393,35 @@ app.post('/change-password', requireAuth, (req, res) => {
   });
 });
 
-//dashboard
-app.get('/', requireAuth, function (req, res) {
-    if (!req.user_id) {
-      res.redirect('/login');
-      return;
+
+app.post('/add-request', upload.single('filename'), (req, res) => {
+  const { user_id, name, description, jabatan, status } = req.body;
+  const filename = req.file ? req.file.filename : null;
+
+  const insertdocumentql = 'INSERT INTO documents (user_id, name, filename, description) VALUES (?, ?, ?, ?)';
+  const documentValues = [user_id, name, filename, description];
+
+  db.query(insertdocumentql, documentValues, (err, documentResult) => {
+    if (err) {
+      throw err;
     }
-    const user_id = req.user_id;
-  
-    const selectUserSql = `SELECT * FROM users WHERE user_id = ${user_id}`;
-    db.query(selectUserSql, (err, userResult) => {
-      if (err) throw err;
-  
-      const selectAllUsersSql = `SELECT * FROM users`;
-      db.query(selectAllUsersSql, (err, allUsersResult) => {
-        if (err) throw err;
-  
-        const selectAllDocumentsSql = `SELECT * FROM documents`;
-        db.query(selectAllDocumentsSql, (err, allDocumentsResult) => {
-          if (err) throw err;
-  
-          const selectAllSignaturesSql = `SELECT * FROM signature`;
-          db.query(selectAllSignaturesSql, (err, allSignaturesResult) => {
-            if (err) throw err;
-  
-            res.render('index', {
-              user: userResult[0],
-              allUsers: allUsersResult,
-              allDocuments: allDocumentsResult,
-              allReq: allSignaturesResult,
-              title: 'Dashboard',
-              layout: 'layouts/main'
-            });
-          });
-        });
-      });
+
+    console.log('success!');
+
+    const documentId = documentResult.insertId;
+    const insertSignatureSql = 'INSERT INTO signature (user_id, document_id, jabatan, status) VALUES (?, ?, ?, ?)';
+    const signatureValues = [user_id, documentId, jabatan, status];
+
+    db.query(insertSignatureSql, signatureValues, (err, signatureResult) => {
+      if (err) {
+        throw err;
+      }
+
+      console.log('success!');
+      res.redirect('/add-document');
     });
   });
+});
 
 
 //port
